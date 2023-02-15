eba5c825c8f23224de3b95dfc9500d28b5e6c80f
ambrosiacvet <97561864+ambrosiacvet@users.noreply.github.com>
2023-02-15 11:51:52 -0600

    Create function to get computer choice in script.js

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let options = ["Rock", "Paper", "Scissors"];
+    let choice = options[Math.floor(Math.random() * 3)];
+
+    return choice;
+};
+
+getComputerChoice();
