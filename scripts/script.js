87ba1cc18e3d75c949a68d78cb34ba1010204fb8
ambrosiacvet <97561864+ambrosiacvet@users.noreply.github.com>
2023-02-15 12:56:56 -0600

    Add round function and game loop to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,47 @@
 function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissors"];
+    let options = ["ROCK", "PAPER", "SCISSORS"];
     let choice = options[Math.floor(Math.random() * 3)];
 
     return choice;
 };
 
-getComputerChoice();
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection == computerSelection) {
+        return "You tie.";
+
+    } else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "SCISSORS" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "ROCK")) {
+        return "You win.";
+
+    } else {
+        return "You loose.";
+    };
+};
+
+function game() {
+
+    let wins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let userInput = prompt("Type rock, paper, or scissors:");
+        let computerInput = getComputerChoice();
+
+        let result = singleRound(userInput, computerInput);
+        console.log(result);
+
+        if (result == "You win.") {
+            wins++;
+        }
+
+    }
+
+    if (wins >= 3) {
+        console.log("You won the game!");
+    } else {
+        console.log("You lost the game. Whoops lol.");
+    }
+
+}
+
+game();
